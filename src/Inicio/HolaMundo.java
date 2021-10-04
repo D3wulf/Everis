2a42b78d0efa435819e07e428a74a5f69ea4bc5d
Miguel Angel <vivanlasfallas@hotmail.com>
2021-10-04 11:51:04 +0200

    Limpio codigo de pruebas

--- src/Inicio/HolaMundo.java
+++ src/Inicio/HolaMundo.java
@@ -5,9 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		
 		String saludo= "Hola a todos";
-		int numero= 1;
-		int numero2 = 3;
-	
+		
 		System.out.println(saludo);
 		
 		
