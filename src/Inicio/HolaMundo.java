ff0a0d913b709e30f82b5ef08f541b93f67afe30
Miguel Angel <vivanlasfallas@hotmail.com>
2021-10-04 11:31:46 +0200

    Primer commit java

---
+++ src/Inicio/HolaMundo.java
@@ -0,0 +1,15 @@
+package Inicio;
+
+public class HolaMundo {
+	
+	public static void main(String[] args) {
+		
+		String saludo= "Hola a todos";
+		
+	
+		System.out.println(saludo);
+		
+		
+	}
+
+}
